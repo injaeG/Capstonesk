72b31d97f812066fa6192975818d81079a87185b
jjune960 <jjune960@gmail.com>
2024-04-14 22:27:37 +0900

    CarMoving

---
+++ Assets/Scenes/Road_Test_Scene/Scripts/CarMoving.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMoving : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public float speed = 0.5f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 0, -speed));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(speed, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, 0, speed));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(-speed, 0, 0));
+        }
+    }
+}
