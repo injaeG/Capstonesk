9c2a7217d0f6e784239d01bf712348cd66257b52
JinH0211 <201814114@office.skhu.ac.kr>
2024-06-07 19:30:59 +0900

    슬렌더 0.1

--- Assets/#NVJOB Slender/Slender/Prefab/Slender.cs
+++ Assets/#NVJOB Slender/Slender/Prefab/Slender.cs
@@ -1,22 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Slender : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the colliding object has the "Car" tag
-        if (other.CompareTag("Car"))
-        {
-            // Destroy the GameObject this script is attached to
-            Destroy(gameObject);
+        // Destroy the GameObject this script is attached to
+        Destroy(gameObject);
 
-            // Reduce fuel amount by 20
-            if (other.GetComponent<VehicleController>() != null)
-            {
-                other.GetComponent<VehicleController>().fuelAmount -= 10f;
-            }
+        // Reduce fuel amount by 20 for any colliding object
+        if (other.GetComponent<VehicleController>() != null)
+        {
+            other.GetComponent<VehicleController>().fuelAmount -= 20f;
         }
     }
 }
